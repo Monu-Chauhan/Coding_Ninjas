83a16e95d11e5e7f3ee38b5abd1916b923d87643
Monu-Chauhan <mc1377929@gmail.com>
2022-11-28 17:34:45 +0530

    Add level Wise Print

--- TreeNode1.cpp
+++ TreeNode1.cpp
@@ -1,73 +1,93 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <queue>
 #include "TreeNode.h"
-TreeNode<int>* takeInputLevelWise()
+TreeNode<int> *takeInputLevelWise()
 {
     int rootdata;
     cout << "Enter root data : ";
     cin >> rootdata;
-    TreeNode<int>* root = new TreeNode<int>(rootdata);
-    queue<TreeNode<int>*> pendingQueue;
+    TreeNode<int> *root = new TreeNode<int>(rootdata);
+    queue<TreeNode<int> *> pendingQueue;
     pendingQueue.push(root);
-    while (pendingQueue.size()!=0)
+    while (pendingQueue.size() != 0)
     {
-        TreeNode<int>* front = pendingQueue.front();
+        TreeNode<int> *front = pendingQueue.front();
         pendingQueue.pop();
-        cout << "Enter number of children of "<< front->data << endl;
+        cout << "Enter number of children of " << front->data << endl;
         int num;
         cin >> num;
-        for(int i=0;i<num;i++)
+        for (int i = 0; i < num; i++)
         {
-            cout << "Enter " << i << "th children of "<<front->data << endl;
+            cout << "Enter " << i << "th children of " << front->data << endl;
             int childData;
             cin >> childData;
-            TreeNode<int>*child = new TreeNode<int>(childData);
+            TreeNode<int> *child = new TreeNode<int>(childData);
             front->children.push_back(child);
             pendingQueue.push(child);
         }
     }
     return root;
-    
 }
-TreeNode<int>* takeInput()
+void printLevelWise(TreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<TreeNode<int> *> pending;
+    pending.push(root);
+    while (pending.size() != 0)
+    {
+        TreeNode<int> *front = pending.front();
+        pending.pop();
+        cout << front->data << ":";
+
+        for (int i = 0; i < front->children.size(); i++)
+        {
+            cout << front->children[i]->data << ",";
+            pending.push(front->children[i]);
+        }
+           cout << endl;
+    }
+}
+TreeNode<int> *takeInput()
 {
     int rootdata;
     cout << "Enter root data : ";
     cin >> rootdata;
-    TreeNode<int>* root = new TreeNode<int>(rootdata);
+    TreeNode<int> *root = new TreeNode<int>(rootdata);
     int n;
-    cout << "Enter number of children of" << rootdata << ":" ;
+    cout << "Enter number of children of" << rootdata << ":";
     cin >> n;
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        TreeNode<int>* child = takeInput();
+        TreeNode<int> *child = takeInput();
         root->children.push_back(child);
     }
     return root;
 }
-void printTree(TreeNode<int>* root)
+void printTree(TreeNode<int> *root)
 {
-    if(root == NULL)
+    if (root == NULL)
     {
         return;
     }
     cout << root->data << ":";
-    for(int i=0;i<root->children.size();i++)
+    for (int i = 0; i < root->children.size(); i++)
     {
         cout << root->children[i]->data << ",";
     }
     cout << endl;
-    for(int i=0; i<root->children.size();i++)
+    for (int i = 0; i < root->children.size(); i++)
     {
         printTree(root->children[i]);
     }
-
 }
 int main()
 {
-    TreeNode<int>* root;
-    
-    root =takeInputLevelWise();
-    printTree(root);
-    //We always delete Tree TODO
+    TreeNode<int> *root;
+
+    root = takeInputLevelWise();
+    printLevelWise(root);
+    // We always delete Tree TODO
 }
